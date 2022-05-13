00184bdd39f3de3a9819a2927b6f5664cba84a51
tim-dustow <tim-dustow@LAPTOP-NFOGHTPT.localdomain>
2022-05-13 12:06:01 +1200

    attempt 1

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,41 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+//----create an array of the images----
+let imgArr = [
+  {
+    name: 'state1',
+    img: './images/unicorn-0.png',
+  },
+  {
+    name: 'state2',
+    img: './images/unicorn-1.png',
+  },
+  {
+    name: 'state3',
+    img: './images/unicorn-2.png',
+  },
+  {
+    name: 'state4',
+    img: './images/unicorn-3.png',
+  },
+]
+let inflation = imgArr[1].img.charAt(imgArr[1].img.length - 5)
 
-console.log("Inflate The Unicorn!")
+console.log(imgArr)
+//create an array of the 3 elements
+let unicornImageArr = document.getElementsByTagName('img')
+let currentImage = unicornImageArr[i]
+for (let i = 0; i < unicornImageArr.length; i++) {
+  unicornImageArr[i].onclick = clickedOn
+}
+console.log(unicornImageArr)
+
+function clickedOn(e) {
+  let inflation = currentImage.src.charAt(currentImage.src.length - 5)
+  if (inflation < 3) {
+    inflation = inflation + 1
+  }
+}
+console.log('inflation is:' + inflation)
+console.log()
