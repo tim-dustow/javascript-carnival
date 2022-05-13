3b6b0d38942464a0bc756f1ce6e38ea3e08bef47
tim-dustow <tim-dustow@LAPTOP-NFOGHTPT.localdomain>
2022-05-13 12:53:54 +1200

    add if statements removed junk: attempt 2

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -2,40 +2,30 @@
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 //----create an array of the images----
-let imgArr = [
-  {
-    name: 'state1',
-    img: './images/unicorn-0.png',
-  },
-  {
-    name: 'state2',
-    img: './images/unicorn-1.png',
-  },
-  {
-    name: 'state3',
-    img: './images/unicorn-2.png',
-  },
-  {
-    name: 'state4',
-    img: './images/unicorn-3.png',
-  },
-]
-let inflation = imgArr[1].img.charAt(imgArr[1].img.length - 5)
-
-console.log(imgArr)
 //create an array of the 3 elements
-let unicornImageArr = document.getElementsByTagName('img')
-let currentImage = unicornImageArr[i]
-for (let i = 0; i < unicornImageArr.length; i++) {
-  unicornImageArr[i].onclick = clickedOn
+let unicornImage = document.getElementsByTagName('img')
+for (let i = 0; i < unicornImage.length; i++) {
+  unicornImage[i].onclick = clickedOn
 }
-console.log(unicornImageArr)
+
+console.log(unicornImage)
 
 function clickedOn(e) {
-  let inflation = currentImage.src.charAt(currentImage.src.length - 5)
-  if (inflation < 3) {
-    inflation = inflation + 1
+  let unicornImage = e.target
+  let mySrc = e.target.src
+  let inflation = mySrc.charAt(mySrc.length - 5)
+  console.log(unicornImage)
+  console.log(inflation)
+  if (inflation === '0') {
+    e.target.src = './images/unicorn-1.png'
+  }
+  if (inflation === '1') {
+    e.target.src = './images/unicorn-2.png'
+  }
+  if (inflation === '2') {
+    e.target.src = './images/unicorn-3.png'
+  }
+  if (inflation === '3') {
+    alert('inflated')
   }
 }
-console.log('inflation is:' + inflation)
-console.log()
