a1666344835a27f5be9e1243ea80b9d60eabc248
tim-dustow <tim-dustow@LAPTOP-NFOGHTPT.localdomain>
2022-05-14 11:52:04 +1200

    add randomNum function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,18 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+let cell = document.getElementsByTagName('td') //---Get individual cells
 
-console.log("Whack-a-Mole!")
+//---get random number 0-24 or 1-25 -1, assign to 'x' array
+function getRandomNumer(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+let hitMoles = 0
+
+function revealMole() {
+  let randomNum = getRandomNumer(0, 25)
+  let randomCell = cell[randomNum]
+}
+
+console.log(cell)
+console.log('Whack-a-Mole!')
