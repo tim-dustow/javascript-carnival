00bae796419c5ce71db857f8db663df019cd14a8
tim-dustow <tim-dustow@LAPTOP-NFOGHTPT.localdomain>
2022-05-15 11:31:40 +1200

    add eventListner

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -2,14 +2,14 @@
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-let bodyPartIndex = -1
+let bodyPartIndex = 0
 
 let clothingIndex = 0
 
 function changeClothes() {
   let headSrc = './images/head' + bodyPartIndex + '.png'
   let bodySrc = './images/body' + bodyPartIndex + '.png'
-  let shoesSrc = './images/shoes' + bodyPartIndex + 'png'
+  let shoesSrc = './images/shoes' + bodyPartIndex + '.png'
 
   let head = document.getElementById('head')
   let body = document.getElementById('body')
@@ -23,5 +23,39 @@ function changeClothes() {
     shoes.src = shoesSrc
   }
 }
+
+document.addEventListener('keydown', function (event) {
+  switch (event.code) {
+    case 'ArrowRight':
+      if (bodyPartIndex < 5) {
+        bodyPartIndex++
+      } else {
+        bodyPartIndex = 0
+      }
+      changeClothes()
+      break
+    case 'ArrowLeft':
+      if (bodyPartIndex < 6 && bodyPartIndex > 0) {
+        bodyPartIndex--
+      } else {
+        bodyPartIndex = 5
+      }
+      changeClothes()
+      break
+    case 'ArrowUp':
+      if (clothingIndex < 2) {
+        clothingIndex++
+      } else {
+        clothingIndex = 0
+      }
+      break
+    case 'ArrowDown':
+      if (clothingIndex < 3 && clothingIndex > 0) {
+        clothingIndex--
+      } else {
+        clothingIndex = 2
+      }
+  }
+})
 
 console.log('Dress The Clown!')
