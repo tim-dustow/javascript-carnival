2da9dfa80210c7853e8300c738d81908ba3974a1
tim-dustow <tim-dustow@LAPTOP-NFOGHTPT.localdomain>
2022-05-14 19:18:55 +1200

    create clothesIndex

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,27 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+let bodyPartIndex = -1
+
+let clothingIndex = 0
+
+function changeClothes() {
+  let headSrc = './images/head' + bodyPartIndex + '.png'
+  let bodySrc = './images/body' + bodyPartIndex + '.png'
+  let shoesSrc = './images/shoes' + bodyPartIndex + 'png'
+
+  let head = document.getElementById('head')
+  let body = document.getElementById('body')
+  let shoes = document.getElementById('shoes')
+
+  if (clothingIndex == 0) {
+    head.src = headSrc
+  } else if (clothingIndex == 1) {
+    body.src = bodySrc
+  } else if (clothingIndex == 2) {
+    shoes.src = shoesSrc
+  }
+}
+
+console.log('Dress The Clown!')
